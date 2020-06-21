1961cbda66d02b4262d4836ef507a54e206d6574
giannishorgos <giannis.21.osfp@gmail.com>
2020-06-21 16:02:29 +0300

    Finish the game, without having a winner

--- main.js
+++ main.js
@@ -1,31 +1,54 @@
 let player = 1;
+let filled = 0;
+let grid = new Array(9);
+grid = document.getElementById("parent").querySelectorAll(".cell");
 
-function game()
+function play_at(cell)
 {
-    if(player == 1)
+    if(filled < 10 && cell.innerHTML == "")
     {
-        this.classList.add("player1");
-        this.innerHTML = "X";
+        if(player == 1)
+        {
+            cell.classList.add("player1");
+            cell.innerHTML = "X";
+            filled++; 
+            player = player == 1 ? 2 : 1;
+            document.getElementById("turn_pl").classList.remove("player1");
+            document.getElementById("turn_pl").classList.add("player2");
+        }
+        else 
+        {
+            cell.classList.add("player2");
+            cell.innerHTML = "O"
+            filled++;   
+            player = player == 1 ? 2 : 1;
+            document.getElementById("turn_pl").classList.remove("player2");
+            document.getElementById("turn_pl").classList.add("player1");
+        }
     }
-    else 
-    {
-        this.classList.add("player2");
-        this.innerHTML = "O"
-    }
+    player == 1 ? 
+        document.getElementById("turn_pl").innerText = "X" : 
+        document.getElementById("turn_pl").innerText = "O"
+
+    check_winner();
 }
 
+function check_winner()
+{
+    for(let i = 0; i < 6; i += 3)
+    {
+        if((grid[i].innerHTML === grid[i + 1].innerHTML && grid[i + 1].innerHTML === grid[i + 2].innerHTML) 
+            && (grid[i].innerHTML === "X" || grid[i].innerHTML === "O"))
+            console.log("winner");
+    }
+}
 function game()
 {
-    let grid = new Array(9);
-    grid = document.getElementById("parent").querySelectorAll(".cell");
-
-    console.log(grid);
-    let filled = 0;
+    console.log(filled);
     for(let i = 0; i < 9; i++)
     {
-        grid[i].addEventListener("click", function () { game(); }
+        grid[i].addEventListener("click", function () { play_at(grid[i]); })
     }
-    console.log(filled);
 }
 
 game();
