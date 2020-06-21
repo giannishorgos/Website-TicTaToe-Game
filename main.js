e5c7598d9c083302c4e8d54510b15ba3dcc421af
giannishorgos <giannis.21.osfp@gmail.com>
2020-06-21 20:27:20 +0300

    the game is ready

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ let filled = 0;
 let grid = new Array(9);
 grid = document.getElementById("parent").querySelectorAll(".cell");
 
-function play_at(cell)
+function playAt(cell)
 {
-    if(filled < 10 && cell.innerHTML == "")
+    if(filled < 10 && cell.innerHTML === "")
     {
-        if(player == 1)
+        if(player === 1)
         {
             cell.classList.add("player1");
             cell.innerHTML = "X";
@@ -26,28 +26,117 @@ function play_at(cell)
             document.getElementById("turn_pl").classList.add("player1");
         }
     }
-    player == 1 ? 
+    player === 1 ? 
         document.getElementById("turn_pl").innerText = "X" : 
         document.getElementById("turn_pl").innerText = "O"
 
-    check_winner();
-}
+    checkWinner();
+} 
 
-function check_winner()
+function checkWinner()
 {
+    for(let i = 0; i < 3; ++i)
+    {
+        if(grid[i].innerHTML === grid[i + 3].innerHTML && grid[i + 3].innerHTML === grid[i + 6].innerHTML
+            && (grid[i].innerHTML === "X" || grid[i].innerHTML === "O"))
+            displayWinner(grid[i].innerHTML);
+    }
     for(let i = 0; i < 6; i += 3)
     {
-        if((grid[i].innerHTML === grid[i + 1].innerHTML && grid[i + 1].innerHTML === grid[i + 2].innerHTML) 
+        if(grid[i].innerHTML === grid[i + 1].innerHTML && grid[i + 1].innerHTML === grid[i + 2].innerHTML
             && (grid[i].innerHTML === "X" || grid[i].innerHTML === "O"))
-            console.log("winner");
+            displayWinner(grid[i].innerHTML);
     }
+    let i = 0;
+    if(grid[i].innerHTML === grid[i + 4].innerHTML && grid[i + 4].innerHTML === grid[i + 8].innerHTML 
+        && (grid[i].innerHTML === "X" || grid[i].innerHTML === "O"))
+        displayWinner(grid[i].innerHTML);
+
+    i = 2;
+    if(grid[i].innerHTML === grid[i + 2].innerHTML && grid[i + 2].innerHTML === grid[i + 4].innerHTML
+        && (grid[i].innerHTML === "X" || grid[i].innerHTML === "O"))
+        displayWinner(grid[i].innerHTML);
 }
+
+function displayWinner(winner)
+{
+    document.getElementById("before").innerText = "WINNER   ";
+    document.getElementById("after").innerText = "";
+    let elem = document.getElementById("turn_pl");
+    elem.innerHTML = winner;
+
+    if(elem.classList.contains("player1"))
+    {
+        elem.classList.remove("player1");
+        elem.classList.add("player2");
+        document.getElementById("before").classList.add("player2");
+        document.getElementById("before").style.fontSize = "50px";
+    } 
+    else 
+    {
+        elem.classList.remove("player2");
+        elem.classList.add("player1");
+        document.getElementById("before").classList.add("player1");
+        document.getElementById("before").style.fontSize = "50px";
+    }
+    retry();
+}
+
+function retry()
+{
+    let btn = document.getElementById("retry");
+    btn.classList.remove("hide");
+    btn.addEventListener("click", reset);
+}
+
+function reset()
+{
+    let elem = document.getElementById("turn_pl");
+    elem.innerText = "X"
+    if(elem.classList.contains("player2"))
+    {
+        elem.classList.add("player1");
+        elem.classList.remove("player2");
+    }
+    let firstMessage = document.getElementById("before");
+    if(firstMessage.classList.contains("player2"))
+    {
+        firstMessage.classList.remove("player2");
+    }
+    else
+    {
+        firstMessage.classList.remove("player1");
+    }
+    firstMessage.innerText = "It's";
+    firstMessage.style.fontSize = 'x-large';
+    let secondMessage = document.getElementById("after");
+    if(secondMessage.classList.contains("player1"))
+    {
+        secondMessage.classList.remove("player1");
+    }
+    else
+    {
+        secondMessage.classList.remove("player2");
+    }
+    secondMessage.innerText = "turn!"
+
+    for(let i = 0; i < 9; ++i)
+    {
+        grid[i].innerHTML = "";
+        grid[i].className = "cell";
+    }
+    player = 1;
+    filled = 0;
+    document.getElementById("retry").classList.add('hide');
+}
+    
+
 function game()
 {
-    console.log(filled);
+    console.log(grid);
     for(let i = 0; i < 9; i++)
     {
-        grid[i].addEventListener("click", function () { play_at(grid[i]); })
+        grid[i].addEventListener("click", function () { playAt(grid[i]); });
     }
 }
 
